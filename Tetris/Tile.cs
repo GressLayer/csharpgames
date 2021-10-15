2b98773cfc4304d67fce76fc2a73adfb6c5424f8
GressLayer <90774865+GressLayer@users.noreply.github.com>
2021-10-15 10:45:21 +0200

    Tiles now draw

--- Tetris/Tile.cs
+++ Tetris/Tile.cs
@@ -9,32 +9,42 @@ namespace Tetris
     {
         public int ColorNumber { get; private set; }
         Color currentColor;
-        public Tile(int color): base("sprites/empty")
+
+        bool isOccupied = false;
+        static string textureBase = "sprites/empty";
+
+        public Tile(int color) : base(textureBase)
         {
+            if (isOccupied)
+                textureBase = "sprites/tetromino";
+            else
+                textureBase = "sprites/empty";
+
             ColorNumber = color;
-            if (ColorNumber == 1)
-                currentColor = Color.LimeGreen;
-            else if (ColorNumber == 2)
-                currentColor = Color.Blue;
-            else if (ColorNumber == 3)
-                currentColor = Color.Red;
-            else if (ColorNumber == 4)
-                currentColor = Color.Purple;
-            else if (ColorNumber == 5)
-                currentColor = Color.Orange;
+
+            if (isOccupied)
+            {
+                switch (ColorNumber)
+                {
+                    case (0): currentColor = Color.White; break;
+                    case (1): currentColor = Color.Orange; break;
+                    case (2): currentColor = Color.Blue; break;
+                    case (3): currentColor = Color.Red; break;
+                    case (4): currentColor = Color.Lime; break;
+                    case (5): currentColor = Color.Cyan; break;
+                    case (6): currentColor = Color.Yellow; break;
+                    case (7): currentColor = Color.Magenta; break;
+                }
+            }
             else
                 currentColor = Color.White;
-
-
-
         }
+
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             {
                 spriteBatch.Draw(sprite, LocalPosition, null, currentColor, 0, origin, 1.0f, SpriteEffects.None, 0);
             }
         }
-
     }
-
 }
