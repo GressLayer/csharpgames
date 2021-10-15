12d209297a71d9821daea71f8848de88135a07c4
Cornetto00 <cornepoepjes@gmail.com>
2021-10-15 11:31:59 +0200

    :I

--- Tetris/BlockShapes/TetrisBlock.cs
+++ Tetris/BlockShapes/TetrisBlock.cs
@@ -23,9 +23,27 @@ namespace Tetris
         }
 
         protected virtual void RotateRight()
-        { }
+        {
+            bool[,] oldState = blockShape;
+            for (int x = 0; x < blockSize; x++)
+            {
+                for (int y = 0; y < blockSize; y++)
+                {
+                    blockShape[x, y] = oldState[3 - y, x];
+                }
+            }
+        }
 
         protected virtual void RotateLeft()
-        { }
+        {
+            bool[,] oldState = blockShape;
+            for(int x = 0; x < blockSize; x++)
+            {
+                for (int y = 0; y < blockSize; y++)
+                {
+                    blockShape[x, y] = oldState[y, 3 - x];
+                }
+            }
+        }
     }
 }
