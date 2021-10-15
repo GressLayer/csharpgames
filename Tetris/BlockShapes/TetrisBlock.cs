9a2a803dda928ae9052b105aff71c6e8a7485e1d
Cornetto00 <cornepoepjes@gmail.com>
2021-10-15 10:45:43 +0200

    :D

---
+++ Tetris/BlockShapes/TetrisBlock.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tetris
+{
+    class TetrisBlock
+    {
+        bool[,] blockShape;
+        const int blockSize = 4;
+
+        public TetrisBlock()
+        {
+            blockShape = new bool[blockSize, blockSize];
+            for (int x = 0; x < blockSize; x++)
+            {
+                for (int y = 0; y < blockSize; y++)
+                {
+                    blockShape[x, y] = false;
+                }
+            }
+        }
+
+        protected virtual void RotateRight()
+        { }
+
+        protected virtual void RotateLeft()
+        { }
+    }
+}
