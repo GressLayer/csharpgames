6a987c54bf747ecb2c154a5ce9beb5fd370c2df2
GressLayer <90774865+GressLayer@users.noreply.github.com>
2021-10-18 14:09:45 +0200

    Blocks are now independent sprites

--- Tetris/BlockObject.cs
+++ Tetris/BlockObject.cs
@@ -8,29 +8,49 @@ namespace Tetris
     class BlockObject : SpriteGameObject
     {
 
-        public int BlockType { get; set; }
-
+        public static int BlockType { get; set; }
+        
         Rectangle spriteRectangle;
-
+        Vector2 rect;
+        
         float angle;
 
-        public BlockObject() : base ("sprites/blocks")
+        static string shape()
         {
-            BlockType = ExtendedGame.Random.Next(7);
-            int index = BlockType;
-            spriteRectangle = new Rectangle(index * sprite.Height / 2, 0, sprite.Height / 2, sprite.Height);
+            if (BlockType == 1) return "sprites/blockL";
+            if (BlockType == 2) return "sprites/blockJ";
+            if (BlockType == 3) return "sprites/blockS";
+            if (BlockType == 4) return "sprites/blockZ";
+            if (BlockType == 5) return "sprites/blockI";
+            if (BlockType == 6) return "sprites/blockO";
+            if (BlockType == 7) return "sprites/blockT";
+            else return "sprites/blockL";
+        }
 
-            LocalPosition = new Vector2(128, -64);
+        public BlockObject() : base(shape())
+        {
+            BlockType = 1 + ExtendedGame.Random.Next(6);
+            int index = BlockType;
+            spriteRectangle = new Rectangle(sprite.Width, sprite.Height, sprite.Width, sprite.Height);
+
+            LocalPosition = new Vector2(192, 64);
             angle = 0f;
+
+            rect = new Vector2(32, 96);
         }
 
         public override void HandleInput(InputHelper inputHelper)
         {
             if (inputHelper.KeyPressed(Keys.Left))
-                LocalPosition = LocalPosition - Movement();
+                LocalPosition = LocalPosition - MovementX();
+
             if (inputHelper.KeyPressed(Keys.Right))
-                LocalPosition = LocalPosition + Movement();
-            if(inputHelper.KeyPressed(Keys.F))
+                LocalPosition = LocalPosition + MovementX();
+
+            if (inputHelper.KeyDown(Keys.Down))
+                LocalPosition = LocalPosition + MovementY();
+
+            if (inputHelper.KeyPressed(Keys.F))
                 angle = angle + (float)Math.PI / 2f;
         }
 
@@ -40,21 +60,28 @@ namespace Tetris
                 localPosition.X = 0;
             if (LocalPosition.X >= 256)
                 localPosition.X = 256;
+            if (LocalPosition.Y >= 640)
+                localPosition.Y = 640;
 
         }
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(sprite, GlobalPosition, spriteRectangle, Color.White, angle, origin, 1.0f, SpriteEffects.None, 0);
+            spriteBatch.Draw(sprite, GlobalPosition, null, Color.White, angle, rect, 1.0f, SpriteEffects.None, 0);
         }
 
         public void DrawNext(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(sprite, new Vector2(624, 184), spriteRectangle, Color.White, angle, origin, 1.0f, SpriteEffects.None, 0);
+            spriteBatch.Draw(sprite, new Vector2(624, 312), null, Color.White, angle, rect, 1.0f, SpriteEffects.None, 0);
         }
 
-        public Vector2 Movement()
+        public Vector2 MovementX()
         {
             return new Vector2(32, 0);
+        }
+
+        public Vector2 MovementY()
+        {
+            return new Vector2(0, 32);
         }
     }
 }
