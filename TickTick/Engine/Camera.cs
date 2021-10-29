455c729f11e39f6c33ee38154ed79c56eacd96c0
Cornetto00 <cornepoepjes@gmail.com>
2021-10-29 12:48:09 +0200

    .

---
+++ TickTick/Engine/Camera.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Engine
+{
+    public class Camera : GameObject
+    {
+        static Rectangle viewWindow;
+
+        int offsetX;
+
+
+        public Camera()
+        {
+            viewWindow = new Rectangle(0, 0, 1024, 768);
+        }
+
+    }
+}
